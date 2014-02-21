fcac920759d77dc9bfd58859f618cbfd31a06ea1
Tri Le <lecaotri.91@gmail.com>
2014-02-21 08:50:37 +0700

    Use Ajax get product from HAL Json to Dojo dialog

--- module/ProductT/src/ProductT/Controller/ProductTController.php
+++ module/ProductT/src/ProductT/Controller/ProductTController.php
@@ -3,19 +3,31 @@ namespace ProductT\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\Config\Reader\Json;
+use Zend\View\Model\JsonModel;
 
 class ProductTController extends AbstractActionController{
 	
 	public function indexAction(){
 		$data = $this->readJson();
 		return array(
-				'category' => $data['category'],
+				'categories' => $data['categories'],
 		);
-	} 
+	}
+	
+	public function getDataAction(){
+		$id = $this->params()->fromQuery('idProduct')-1;
+		$data = $this->readJson();
+		$product = $data['categories'][$id]['_embedded']['products'][0];
+		$result = new JsonModel(array(
+				'product' => $product,
+				'success' => true,
+		));
+		return $result;
+	}
 	
 	private function readJson(){
 		$reader = new Json();
-		$data = $reader->fromFile('./json/product.json');
+		$data = $reader->fromFile('./json/hal_product.json');
 		return $data;
 	}
 }
